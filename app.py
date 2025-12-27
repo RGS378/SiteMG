21e9eaec4990118b8648660370bc34226add797e
Gabriel <gabriel.aguiar.fidelis13@gmail.com>
2025-12-27 14:11:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("inicialPage/inicio.html")
 
+@app.route("/cursos")
+def cursos():
+    return render_template("secondPages/cursos.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("secondPages/contato.html")
+
+@app.route("/galeria")
+def galeria():
+    return render_template("secondPages/galeria.html")
+
+@app.route("/noticia")
+def noticia():
+    return render_template("secondPages/noticia.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
