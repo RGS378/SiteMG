75eedba4cbff7ef1e97c501dc0a3ab2df2fbf466
Gabriel <gabriel.aguiar.fidelis13@gmail.com>
2025-12-27 14:00:57 -0300

    Atualização 2.2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("/inicialPage/index.html")
+    return render_template("inicialPage/inicio.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
